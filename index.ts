842b76eb6fdc326a2c31d2cd86e84f67373e2998
Rotem Meiri <rotemx@storemaven.com>
2019-08-18 04:22:24 +0300

    added index for npmˆ

---
+++ index.ts
@@ -0,0 +1,11 @@
+import {Entity} from "./src/decorators/entity/entity-decorator";
+import {Model} from "./src/abstract/Model";
+import {hasOne} from "./src/decorators/has-one/has-one-decorator";
+import {hasMany} from "./src/decorators/has-many/has-many-decorator";
+
+exports = {
+	Entity,
+	Model,
+	hasOne,
+	hasMany
+}
