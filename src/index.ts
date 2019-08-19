72ad369def633fe3fc66a46ccef09d379a2c11b2
Rotem Meiri <rotemx@storemaven.com>
2019-08-19 20:06:51 +0300

    before publish

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export {Entity} from "./decorators/entity/entity-decorator";
 export {Model} from "./model/Model";
 export {hasOne} from "./decorators/has-one/has-one-decorator";
 export {hasMany} from "./decorators/has-many/has-many-decorator";
-
+export {field} from "./decorators/field/field-decorator"
