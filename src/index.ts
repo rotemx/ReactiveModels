29dcec76ab385b33d707b9694ecd0e9019dc6a84
Rotem Meiri <rotemx@storemaven.com>
2019-08-19 20:00:14 +0300

    before publish

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
-import {Entity} from "./decorators/entity/entity-decorator";
-import {Model} from "./model/Model";
-import {hasOne} from "./decorators/has-one/has-one-decorator";
-import {hasMany} from "./decorators/has-many/has-many-decorator";
+export {Entity} from "./decorators/entity/entity-decorator";
+export {Model} from "./model/Model";
+export {hasOne} from "./decorators/has-one/has-one-decorator";
+export {hasMany} from "./decorators/has-many/has-many-decorator";
 
-exports = {
-	Entity,
-	Model,
-	hasOne,
-	hasMany
-}
